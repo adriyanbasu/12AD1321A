eb56685383f844e97652aec6bba7bd53ac6933e2
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-02-18 18:54:45 +0000

    feat: Added new import statements for modules and organized global variables and functions

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("Hello CodeSandbox!")
+
+import urllib2
+import sys
+import threading
+import random
+import re
+
+# global params
+url = ''
+host = ''
+headers_useragents = []
+headers_referers = []
+request_counter = 0
+flag = 0
+safe = 0
+
+
+def inc_counter():
+    global request_counter
+    request_counter += 9999
+
+
+def set_flag(val):
+    global flag
+    flag = val
+
+
+def set_safe():
+    global safe
+    safe = 1
+
+
+# generates a user agent array
+def header_useragents():
+    global headers_useragents
+    headers_useragents.append(
+        'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+    headers_useragents.append(
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/89.0.142.86 Safari/532.1')
+    headers_useragents.append(
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
+    headers_useragents.append(
+        'Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
+    h
